d632f4ba847acc891b6b9ebc4c18216c8074105a
Antonio-hash <saofsaof6@gmail.com>
2021-06-28 19:27:33 -0500

    cambios firebase

--- ALIMENTADORES/index.js
+++ ALIMENTADORES/index.js
@@ -1,25 +1,30 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyBJYkf1vdBb6i6TkZgvy2jDU-qT5REf8Fw",
-    authDomain: "preyecto-iot.firebaseapp.com",
-    databaseURL: "https://preyecto-iot-default-rtdb.firebaseio.com/",
-    projectId: "preyecto-iot",
-    storageBucket: "preyecto-iot.appspot.com",
-    messagingSenderId: "727817961430",
-    appId: "1:727817961430:web:6453cb22169300a9a02e77"
-  };
-  firebase.initializeApp(firebaseConfig);
+// var firebaseConfig = {
+//     apiKey: "AIzaSyBJYkf1vdBb6i6TkZgvy2jDU-qT5REf8Fw",
+//     authDomain: "preyecto-iot.firebaseapp.com",
+//     databaseURL: "https://preyecto-iot-default-rtdb.firebaseio.com/",
+//     projectId: "preyecto-iot",
+//     storageBucket: "preyecto-iot.appspot.com",
+//     messagingSenderId: "727817961430",
+//     appId: "1:727817961430:web:6453cb22169300a9a02e77"
+//   };
+//   firebase.initializeApp(firebaseConfig);
 
 
-var app = document.getElementById('app')
+// var app = document.getElementById('app')
 
-var dbRef = firebase.database()
+// var dbRef = firebase.database()
 
-var messageRef = dbRef.ref('alimentador1')
-
-
-messageRef.once('value').then(function(snap){
-    app.innerText = snap.val()
-})
+// var messageRef = dbRef.ref('alimentador1')
+// var messageRef = dbRef.ref('alimentador2')
 
 
-///estamos perdidos jajaaja
+// messageRef.once('value').then(function(snap){
+//     app.innerText = snap.val()
+// })
+
+// messageRef.once('value').then(function(snap){
+//   app.innerText = snap.val()
+// })
+
+
+
